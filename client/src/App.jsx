c83fb8adac2554787dfa73462bdefe6d844bb696
vikramdev772 <vikramdev772@gmail.com>
2026-01-09 15:10:02 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./App.css"
+import { Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <>
-    <div>App</div>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={}/>
+      </Routes>
+    </Router>
     </>
   )
 }
